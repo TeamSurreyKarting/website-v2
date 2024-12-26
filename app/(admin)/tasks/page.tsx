308c311838003a05304f06b7c6d3c05ad76ce9c6
George Nick Gorzynski <georgegorzynski@me.com>
2024-12-26 23:09:53 +0000

    Create `tasks` page

--- app/(admin)/tasks/page.tsx
+++ app/(admin)/tasks/page.tsx
@@ -1,7 +1,46 @@
-export default function TasksListPage() {
+import Search from "@/components/search";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import { FaPlus } from "react-icons/fa6";
+import TaskCharts from "@/components/tasks/tasks-charts";
+import { Suspense } from "react";
+
+export default async function TasksPage({ searchParams }: {
+	searchParams?: Promise<{
+		q?: string;
+		view?: string;
+	}>;
+}) {
+	const query = (await searchParams)?.q ?? "";
+	const view = (await searchParams)?.view;
+
 	return (
-		<>
-			<h1>Tasks</h1>
-		</>
+		<div
+			className={"container mx-auto"}
+		>
+			<h2 className={"text-2xl font-bold"}>Tasks</h2>
+			<div className="mx-auto my-2 flex justify-between gap-x-2">
+				<Search />
+				<Link
+					href={"/tasks/new"}
+					className={"flex gap-2 items-center"}
+				>
+					<Button
+						variant={"secondary"}
+					>
+						<FaPlus />
+						Create
+					</Button>
+				</Link>
+			</div>
+			<Suspense
+				key={query}
+				fallback={
+					<p>Loading...</p>
+				}
+			>
+				<TaskCharts query={query} view={view} />
+			</Suspense>
+		</div>
 	);
 }
