024ef5725d0ace936bdddd9138309db889306fa1
George Nick Gorzynski <georgegorzynski@me.com>
2024-12-26 23:08:37 +0000

    Create component to fetch and filter tasks, and create component with view switcher and data views

---
+++ components/tasks/tasks-charts.tsx
@@ -0,0 +1,50 @@
+import ChartViewSwitcher from "@/components/tasks/chart-view-switcher";
+import {createClient} from "@/utils/supabase/server";
+import TasksDataTable from "@/components/tasks/data-table/data-table";
+import clsx from "clsx";
+import {ChartViewType, isChartViewType} from "@/utils/types/chart-view-type";
+import TasksKanbanBoard from "@/components/tasks/kanban-board/board";
+
+async function getTasks(query?: string) {
+	const supabase = await createClient();
+
+	const dbQuery = supabase.from("Tasks").select();
+
+	if (query) {
+		dbQuery.like("title", `%${query}%`);
+
+		// todo: include filtering by description
+		// dbQuery.like("description", `%${query}%`);
+	}
+
+	const { data: tasks, error } = await dbQuery;
+
+	if (error) throw error;
+
+	return tasks;
+}
+
+export default async function TaskCharts({ query, view }: { query?: string, view?: string, }) {
+	const tasks = await getTasks(query);
+
+	const viewType: ChartViewType = isChartViewType(view) ? ChartViewType[view] : ChartViewType.kanban;
+
+	return (
+		<>
+			<ChartViewSwitcher defaultValue={viewType} />
+			<TasksKanbanBoard
+				tasks={tasks}
+				className={clsx({
+					"grid": view === ChartViewType.kanban,
+					"hidden": view !== ChartViewType.kanban,
+				})}
+			/>
+			<div className={clsx({
+				"block": view === ChartViewType.list,
+				"hidden": view !== ChartViewType.list,
+			})}>
+				<TasksDataTable tasks={tasks}/>
+			</div>
+		</>
+	);
+}
