25cf388ec001894aa505f6425ca7021df2fd087c
George Nick Gorzynski <georgegorzynski@me.com>
2025-02-25 00:13:33 +0000

    Add headers and titles, ensure selected time stays focused

--- components/ui/datetime-picker.tsx
+++ components/ui/datetime-picker.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "@/hooks/use-media-query";
-import { format } from "date-fns";
+import { format, isAfter, isBefore, startOfDay, endOfDay } from "date-fns";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Calendar } from "@/components/ui/calendar";
 import { CalendarClock, CalendarIcon, ClockIcon } from "lucide-react";
 import clsx from "clsx";
@@ -28,8 +28,8 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
 }) => {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [open, setOpen] = useState(false);
-  const [dateSelected, setDateSelected] = useState<Date | undefined>();
-  const [timeIsDirty, setTimeIsDirty] = useState<boolean>(false);
+  const [dateSelected, setDateSelected] = useState<Date | undefined>(datetime);
+  const [timeIsDirty, setTimeIsDirty] = useState<boolean>(datetime !== undefined);
   const [activeTab, setActiveTab] = useState<"date" | "time" | "summary">("date");
 
   const handleDateSelect = (value: Date | undefined) => {
@@ -52,39 +52,34 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
     setOpen(false);
   }
 
-  // The overlay content (shared between Sheet and Dialog)
   const PickerContent = () => (
     <div className="p-4">
-      <div className="flex space-x-2 mb-4 items-center">
-        {(() => {
-          switch (activeTab) {
-            case "date":
-              return <CalendarIcon className="mr-2 h-4 w-4" />
-            case "time":
-              return <ClockIcon className="mr-2 h-4 w-4" />
-            case "summary":
-              return <CalendarClock className="mr-2 h-4 w-4" />
-          }
-        })()}
-        <h4>
-          {(() => {
-            switch (activeTab) {
-              case "date":
-                return "Select Date"
-              case "time":
-                return "Select Time"
-              case "summary":
-                return "Review Date and Time"
-            }
-          })()}
-        </h4>
-      </div>
       {(() => {
         switch (activeTab) {
           case "date":
-            return (<Calendar mode={"single"} selected={dateSelected} onSelect={handleDateSelect} />);
+            return (
+              <Calendar
+                mode={"single"}
+                selected={dateSelected}
+                onSelect={handleDateSelect}
+                disabled={(date) => {
+                  const dateIsBeforeMin = (min && isBefore(date, startOfDay(min))) ?? false;
+                  const dateIsAfterMax = (max && isAfter(date, endOfDay(max))) ?? false;
+
+                  return dateIsBeforeMin || dateIsAfterMax;
+                }}
+              />
+            );
           case "time":
-            return (<TimeList intervalSeconds={intervalSeconds} dateSelected={dateSelected} onTimeSelect={handleTimeSelect} />);
+            return (
+              <TimeList
+                intervalSeconds={intervalSeconds}
+                dateSelected={dateSelected}
+                onTimeSelect={handleTimeSelect}
+                min={min}
+                max={max}
+              />
+            );
           case "summary":
             return (
               <div className={"flex flex-col gap-2 mt-4 mb-8"}>
@@ -156,6 +151,32 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
             <TriggerButton />
           </SheetTrigger>
           <SheetContent side={"bottom"}>
+            <SheetHeader>
+              <SheetTitle className={"flex flex-row gap-2 items-center"}>
+                {(() => {
+                  switch (activeTab) {
+                    case "date":
+                      return <CalendarIcon className="mr-2 h-4 w-4" />
+                    case "time":
+                      return <ClockIcon className="mr-2 h-4 w-4" />
+                    case "summary":
+                      return <CalendarClock className="mr-2 h-4 w-4" />
+                  }
+                })()}
+                <span>
+                  {(() => {
+                    switch (activeTab) {
+                      case "date":
+                        return "Select Date"
+                      case "time":
+                        return "Select Time"
+                      case "summary":
+                        return "Review Date and Time"
+                    }
+                  })()}
+                </span>
+              </SheetTitle>
+            </SheetHeader>
             <PickerContent />
           </SheetContent>
         </Sheet>
@@ -165,6 +186,30 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
             <TriggerButton />
            </DialogTrigger>
            <DialogContent>
+             <DialogHeader>
+               <DialogTitle className={"flex flex-row gap-2 items-center"}>
+                 {(() => {
+                   switch (activeTab) {
+                     case "date":
+                       return <CalendarIcon className="mr-2 h-4 w-4" />
+                     case "time":
+                       return <ClockIcon className="mr-2 h-4 w-4" />
+                     case "summary":
+                       return <CalendarClock className="mr-2 h-4 w-4" />
+                   }
+                 })()}
+                {(() => {
+                  switch (activeTab) {
+                    case "date":
+                      return "Select Date"
+                    case "time":
+                      return "Select Time"
+                    case "summary":
+                      return "Review Date and Time"
+                  }
+                })()}
+               </DialogTitle>
+             </DialogHeader>
              <PickerContent />
            </DialogContent>
          </Dialog>
@@ -201,18 +246,30 @@ const TimeList: React.FC<TimeListProps> = ({ intervalSeconds, dateSelected, onTi
     times.push(new Date(currentTime));
   }
 
+  const selectedTimeRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    if (selectedTimeRef.current) {
+      selectedTimeRef.current.scrollIntoView({ behavior: "instant", block: "center" });
+    }
+  }, [dateSelected]);
+
   return (
     <div className="flex flex-col gap-2 max-h-80 overflow-auto">
       {times.map((time, index) => {
         const isActive = time.getTime() === dateSelected?.getTime();
 
         return (
-          <Button key={index} variant={isActive ? "default" : "secondary"} onClick={() => onTimeSelect(time)}>
+          <Button
+            key={index}
+            ref={isActive ? selectedTimeRef : null}
+            variant={isActive ? "default" : "secondary"}
+            onClick={() => onTimeSelect(time)}
+          >
             {format(time, "p")}
           </Button>
-        )
-      }
-      )}
+        );
+      })}
     </div>
   );
 };
